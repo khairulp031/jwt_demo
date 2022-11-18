18f9d8b05b7186c7f2281cd767e3a64c7ba9d5e2
khairul <halo@khairulanshar.com>
2022-11-19 02:40:40 +0800

    initial

---
+++ src/main/java/com/example/jwt_demo/services/Simple.java
@@ -0,0 +1,24 @@
+package com.example.jwt_demo.services;
+
+import com.example.jwt_demo.utils.PublicKeyReader;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.util.FileCopyUtils;
+import org.springframework.util.ResourceUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/api/simple/v1")
+public class Simple {
+
+
+    @GetMapping()
+    String getsimple(@RequestHeader("authorization") String authorization) throws Exception {
+        return PublicKeyReader.verify(authorization.replace("Bearer ",""));
+    }
+}
---
+++ src/main/java/com/example/jwt_demo/utils/PublicKeyReader.java
@@ -0,0 +1,55 @@
+package com.example.jwt_demo.utils;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.auth0.jwt.interfaces.JWTVerifier;
+import org.springframework.util.FileCopyUtils;
+import org.springframework.util.ResourceUtils;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.security.KeyFactory;
+import java.security.interfaces.RSAPublicKey;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.Base64;
+
+public class PublicKeyReader {
+
+    static RSAPublicKey rsaPublicKey;
+    private static RSAPublicKey getRSAPublicKey() throws Exception {
+        if (rsaPublicKey !=null) {
+            return rsaPublicKey;
+        }
+        File file = ResourceUtils.getFile("classpath:cert/rsa.pub.pem");
+        InputStream in = new FileInputStream(file);
+        byte[] bdata = FileCopyUtils.copyToByteArray(in);
+        String publicKey = new String(bdata, StandardCharsets.UTF_8)
+                .replace("-----BEGIN PUBLIC KEY-----","")
+                .replace("-----END PUBLIC KEY-----","")
+                .replace("\n", "")
+                .replace("\r", "");
+        //System.out.println(publicKey);
+        byte[] publicKeyByteArr = Base64.getDecoder().decode(publicKey);
+        KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+        rsaPublicKey = (RSAPublicKey) keyFactory.generatePublic(new X509EncodedKeySpec(publicKeyByteArr));
+        return rsaPublicKey;
+    }
+
+    public static String verify(String token) {
+        try {
+            RSAPublicKey rsaPublicKey = getRSAPublicKey();
+            Algorithm algorithm = Algorithm.RSA512(rsaPublicKey, null);
+            JWTVerifier verifier = JWT.require(algorithm)
+                    .build();
+            DecodedJWT verifiedJWT = verifier.verify(token);
+            Base64.Decoder decoder = Base64.getUrlDecoder();
+            String[] chunks = token.split("\\.");
+            String payload = new String(decoder.decode(chunks[1]));
+            return payload;
+        } catch (Exception e) {
+          return e.toString();
+        }
+
+    }
+}
